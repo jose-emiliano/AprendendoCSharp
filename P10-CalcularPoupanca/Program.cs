0ced924b3ec37feb69535547050bb2bd72829507
José Emiliano <92037584+jose-emiliano@users.noreply.github.com>
2023-03-05 19:11:07 -0300

    Projeto 10 - Calcular Poupaça

---
+++ P10-CalcularPoupanca/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    double investimento = 1000;
+    double juros = 0.5/100;
+    int mes = 1;
+
+    while (mes <= 12)
+    {
+      investimento += investimento * juros;
+      Console.WriteLine("No mes " + mes + " você tem R$" + investimento + ".");
+      
+      mes++;
+    }
+  }
+}
