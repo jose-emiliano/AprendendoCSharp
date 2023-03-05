edb0002544d54c11a3bdb4d009e5ab83e53d18e3
José Emiliano <92037584+jose-emiliano@users.noreply.github.com>
2023-03-05 20:21:03 -0300

    Encadeando Fors e utilizando break

---
+++ P12-InvestindoALongoPrazo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    double fatorDeRendimento = 1.005;
+    double investimento = 1000;
+
+    for (int anos = 1; anos <= 5; anos++)
+    {
+      for (int mes = 1; mes <= 12; mes++)
+      {
+        investimento *= fatorDeRendimento;
+      }
+
+      fatorDeRendimento += 0.001;
+    }
+  
+    Console.WriteLine("Depois de 5 anos você terá R$" + investimento + ".");
+  }
+
+}
---
+++ P13-EncadeandoFor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    // Sem break
+    for (int contadorLinhas = 0; contadorLinhas <= 10; contadorLinhas++)
+    {
+      for (int contadorColunas = 0; contadorColunas <= contadorLinhas; contadorColunas++)
+      {
+        Console.Write("*");
+      }
+      Console.WriteLine();
+    }
+
+    // Com break
+    for (int contadorLinhas = 0; contadorLinhas <= 10; contadorLinhas++)
+    {
+      for (int contadorColunas = 0; contadorColunas <= 10; contadorColunas++)
+      {
+        Console.Write("*");
+        if (contadorColunas >= contadorLinhas) break;
+      }
+      Console.WriteLine();
+    }
+
+  }
+}
