697b56d7280f12ee77e569a556988f18639b542b
José Emiliano <92037584+jose-emiliano@users.noreply.github.com>
2023-03-05 20:30:55 -0300

    Desafio02-MultiploDe3

---
+++ Desafio02-MultiploDe3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+
+    for (int numero = 1; numero <= 100; numero++)
+    {
+      if(numero % 3 == 0)
+      {
+        Console.WriteLine(numero);
+      }
+    }
+  }
+}
