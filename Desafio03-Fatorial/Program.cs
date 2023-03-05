0dada0b3d0d988cce92d65cca7f76866d5e3f0e7
José Emiliano <92037584+jose-emiliano@users.noreply.github.com>
2023-03-05 20:40:31 -0300

    Desafio03 - Fatorial

---
+++ Desafio03-Fatorial/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+  static void Main (string[] args)
+  {
+    int fator = 1;
+    
+    for (int i = 1; i <= 10; i ++)
+    {
+      fator *= i;
+      Console.WriteLine("O fator de " + i + " é " + fator + ".");
+    }
+  }
+}
